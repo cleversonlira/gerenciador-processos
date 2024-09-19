79f0623820bdc050e62d8aa38df678ece26e7b39
lira <cleverson@hipcomp.com.br>
2024-09-19 20:44:52 -0300

    Implementacao de tratamento de erros nas respostas.

---
+++ src/main/java/com/pol/gerenciador_processos/application/config/error/Error.java
@@ -0,0 +1,13 @@
+package com.pol.gerenciador_processos.application.config.error;
+
+public class Error {
+
+    public String erro;
+    public String field;
+
+    public Error(String erro, String field) {
+        this.erro = erro;
+        this.field = field;
+    }
+
+}
